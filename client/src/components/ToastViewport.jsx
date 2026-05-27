8549c8f15a94c5b317c9e86ae28100d2e8c6274b
HamzaKhaliq921 <abdulkhaliq920285@gmail.com>
2026-05-26 21:57:26 -0600

    const toneClasses

---
+++ client/src/components/ToastViewport.jsx
@@ -0,0 +1,5 @@
+const toneClasses = {
+  success: 'border-emerald-200/70 bg-emerald-50/85 text-emerald-900',
+  error: 'border-rose-200/70 bg-rose-50/85 text-rose-900',
+  info: 'border-sage-200/70 bg-white/85 text-sage-900',
+};
