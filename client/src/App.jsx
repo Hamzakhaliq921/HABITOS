151ed2117bd4ed10f0fac25923c81f37cd56e416
HamzaKhaliq921 <abdulkhaliq920285@gmail.com>
2026-05-26 21:59:36 -0600

    import all things in app.jsx file

---
+++ client/src/App.jsx
@@ -0,0 +1,7 @@
+import { useRef, useState } from 'react';
+import { DashboardShell } from './components/DashboardShell';
+import { ReportExportDocument } from './components/ReportExportDocument';
+import { ToastViewport } from './components/ToastViewport';
+import { useHabitsDashboard } from './hooks/useHabitsDashboard';
+import { usePrayerTracker } from './hooks/usePrayerTracker';
+import { useTheme } from './hooks/useTheme';
