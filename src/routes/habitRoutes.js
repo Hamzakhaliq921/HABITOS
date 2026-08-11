1f15b57cd77f4f90e2ce23902e1325a88c4fa9f8
HamzaKhaliq921 <abdulkhaliq920285@gmail.com>
2026-08-11 13:32:30 -0600

    export all router

--- src/routes/habitRoutes.js
+++ src/routes/habitRoutes.js
@@ -15,3 +15,13 @@ const {
   validateCompletionBody
 } = require("../middleware/validateRequest");
 
+const router = express.Router();
+
+router.get("/analytics", getAnalyticsData);
+router.get("/streaks", getStreakData);
+router.route("/").post(validateHabitBody, createHabit).get(getAllHabits);
+router.put("/:id", validateObjectId, validateHabitBody, updateHabit);
+router.delete("/:id", validateObjectId, deleteHabit);
+router.patch("/:id/complete", validateObjectId, validateCompletionBody, markHabitComplete);
+
+module.exports = router;
