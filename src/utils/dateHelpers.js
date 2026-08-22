4e6d451881487da8338d9f96ec01437cab1806f4
HamzaKhaliq921 <abdulkhaliq920285@gmail.com>
2026-08-21 22:55:37 -0600

    differenceInDays like earlier and later

--- src/utils/dateHelpers.js
+++ src/utils/dateHelpers.js
@@ -13,3 +13,6 @@ const formatDateKey = (dateInput = new Date()) => {
 const differenceInDays = (laterDateKey, earlierDateKey) => {
   const later = new Date(`${laterDateKey}T00:00:00.000Z`);
   const earlier = new Date(`${earlierDateKey}T00:00:00.000Z`);
+
+  return Math.round((later - earlier) / MS_PER_DAY);
+};
