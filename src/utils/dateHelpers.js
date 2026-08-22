1aea8791d14df1fbd6d19b6736b78686a7617b77
HamzaKhaliq921 <abdulkhaliq920285@gmail.com>
2026-08-21 22:55:00 -0600

    differenceInDays

--- src/utils/dateHelpers.js
+++ src/utils/dateHelpers.js
@@ -10,3 +10,6 @@ const formatDateKey = (dateInput = new Date()) => {
   return date.toISOString().split("T")[0];
 };
 
+const differenceInDays = (laterDateKey, earlierDateKey) => {
+  const later = new Date(`${laterDateKey}T00:00:00.000Z`);
+  const earlier = new Date(`${earlierDateKey}T00:00:00.000Z`);
